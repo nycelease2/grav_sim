cb10b80f66ed1b75c8708a4730b30b6f8e31164a
nycelease2 <raaghav@thapliyal.net>
2025-11-19 08:25:56 +0530

    expanded input

--- src/main.py
+++ src/main.py
@@ -16,13 +16,17 @@ class planet:
 #taking input for planets
 print("Hello user, this is a 2 planet system gravity simulator, please input the prefered values")
 print("for your information, screen size is 800 by 600")
-p1_r = int(input("radius of first planet: "))
-p1_x = int(input("x coordinate: "))
-p1_y = int(input("y coordinate: "))
+p1_r = float(input("radius of first planet: "))
+p1_x = float(input("x coordinate: "))
+p1_y = float(input("y coordinate: "))
+p1_xvel = float(input("x velocity: "))
+p1_yvel = float(input("y velocity: "))
 
-p2_r = int(input("radius of second planet: "))
-p2_x = int(input("x coordinate: "))
-p2_y = int(input("y coordinate: "))
+p2_r = float(input("radius of second planet: "))
+p2_x = float(input("x coordinate: "))
+p2_y = float(input("y coordinate: "))
+p2_xvel = float(input("x velocity: "))
+p2_yvel = float(input("y velocity: "))
 
 T = int(input("prefered time multiplier(I reccomend 1000): "))
 
@@ -33,8 +37,8 @@ clock = pygame.time.Clock()
 running = True
 
 #making planets
-p1 = planet(p1_r, "red", p1_x, p1_y, pygame.math.Vector2())
-p2 = planet(p2_r, "white", p2_x, p2_y, pygame.math.Vector2())
+p1 = planet(p1_r, "red", p1_x, p1_y, pygame.math.Vector2(p1_xvel, p1_yvel))
+p2 = planet(p2_r, "white", p2_x, p2_y, pygame.math.Vector2(p2_xvel, p2_yvel))
 
 bodies = [p1,p2]
 
